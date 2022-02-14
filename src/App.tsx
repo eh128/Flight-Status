9202c67ea978776c8d2dc9c3a30399b15a46a33e
Ethan Hsu <ethanhsu128@gmail.com>
2022-02-13 22:09:45 -0500

    add params

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  fetch(
-    `http://api.aviationstack.com/v1/flights?access_key=5ef166aeb3a6780fccdfa37793d81651`
-  )
+  let params = new URLSearchParams({
+    access_key: "5ef166aeb3a6780fccdfa37793d81651",
+    limit: "2",
+  });
+  fetch(`http://api.aviationstack.com/v1/flights?${params}`)
     .then((res) => res.json())
     .then(console.log);
   return (
