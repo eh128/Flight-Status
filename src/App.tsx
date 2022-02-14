944eec4a34cbda78623091b189d3b757336aa26e
Ethan Hsu <ethanhsu128@gmail.com>
2022-02-13 22:04:33 -0500

    api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  fetch(
+    `http://api.aviationstack.com/v1/flights?access_key=5ef166aeb3a6780fccdfa37793d81651`
+  )
+    .then((res) => res.json())
+    .then(console.log);
+  return (
+    <div className="App">
+      <div>Flights</div>
+    </div>
+  );
 }
 
 export default App;
